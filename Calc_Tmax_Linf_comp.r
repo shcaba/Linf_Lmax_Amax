cee3168b9e5724e191deba06dbff5b0c61f30b3b
shcaba <jason.cope@noaa.gov>
2025-04-04 17:24:08 -0700

    update with new sims and plots

--- Calc_Tmax_Linf_comp.r
+++ Calc_Tmax_Linf_comp.r
@@ -7,10 +7,12 @@ library(FSAsim)
 library(FSA)
 library(EnvStats)
 library(reshape2)
-
+library(viridis)
+source()
 ### Functions ###
 VBGF<-function(Linf, k, t0, ages){ 
   Lts<-Linf * (1 - exp(-k * (ages - t0)))
+  Lts
 }
 
 rand.VBGF<-function(Linf, k, t0, ages,CV){ 
@@ -26,6 +28,87 @@ vbgf.inflect<-function(Linf,k,t0)
 VBGF.age<-function(Linf,k,t0,lt){ 
   t0 - (log(1 - (lt / Linf)) / k) 
 } 
+
+trunc.rand.VBGF<-function(Linf, k, t0, ages,CV,maxlt){ 
+  Lts<-Linf * (1 - exp(-k * (ages - t0)))
+  rlnormTrunc(length(Lts), Lts, CV*Lts, min = 0,max = maxlt)
+  #rnorm(length(Lts),Lts,CV*Lts) 
+}
+
+Comp.Lmax.Linf<-function(n=100,Nsim=500,Linf.in,K.in,t0.in,CV.in,Amax.in,lowage,maxage,maxlt)
+{
+  Linf_Lmax_ratios<-data.frame(Lmax_TLinf=NA,Lmax_estLinf=NA,estLinf_TLinf=NA,Linf.true=NA,Linf_est=NA,Lmax=NA, Amin=NA, Amax=NA,Kest=NA,K_jit=NA,toest=NA,t0_jit=NA)
+  for(i in 1:Nsim)
+  {
+    #Create data
+    rand.ages<-rlnormTrunc(n, meanlog = log(Amax.in), sdlog = 1, min = lowage,max = maxage)
+    rand.lts<-rand.VBGF(Linf.in,K.in,t0.in,rand.ages,CV.in)
+    age.lt.dat<-data.frame(Ages=round(rand.ages,0),Lengths=round(rand.lts,0))
+    #Filter max length
+    age.lt.dat<-age.lt.dat[age.lt.dat$Lengths<maxlt,]
+    
+    
+    
+    #  Age_Lts<-data.frame(Ages=rand.ages,Lts=rand.lts)
+    #  plot(rand.ages,rand.lts)
+    #Fit VBGF curve
+    k_t0_jits<-c(jitter(K.in,amount=0.05),jitter(t0.in,amount=0.05))
+    growth.out<-try(nls(age.lt.dat$Lengths~Linf * (1 - exp(-k * (rand.ages - t0))),
+                        data=age.lt.dat,start=list(Linf=max(rand.lts),k=k_t0_jits[1],t0=0)))
+    #growth.out<-try(growth(size=rand.lts,age=round(rand.ages,0),Sinf=max(rand.lts),K=k_t0_jits[1],t0=0,
+    #                       graph=FALSE,control=list(maxiter=10000,tol=1e-5))) #k_t0_jits[2]
+    #vbmod <- Lts ~ Linf * (1 - exp(-K * (Ages - t0)))
+    #vbgf.parms<-list(Linf=20,K=0.1,t0=0)
+    #vbgf.fit<-nls(vbmod,data=Age_Lts,start=vbgf.parms)
+    if(class(growth.out)=="try-error")
+    {Linf_Lmax_ratios[i,]=NA}
+    
+    if(class(growth.out)=="nls")
+    {
+      growth.out.parms<-growth.out$m$getPars()
+      print(growth.out.parms)
+      Linf_Lmax_ratios[i,1]<-max(age.lt.dat$Lengths)/Linf.in
+      Linf_Lmax_ratios[i,2]<-max(age.lt.dat$Lengths)/growth.out.parms[1]
+      Linf_Lmax_ratios[i,3]<-growth.out.parms[1]/Linf.in
+      Linf_Lmax_ratios[i,4]<-Linf.in
+      Linf_Lmax_ratios[i,5]<-growth.out.parms[1]
+      Linf_Lmax_ratios[i,6]<-max(age.lt.dat$Lengths)
+      Linf_Lmax_ratios[i,7]<-min(age.lt.dat$Ages)
+      Linf_Lmax_ratios[i,8]<-max(age.lt.dat$Ages)
+      Linf_Lmax_ratios[i,9]<-growth.out.parms[2]
+      Linf_Lmax_ratios[i,10]<-k_t0_jits[1]
+      Linf_Lmax_ratios[i,11]<-growth.out.parms[3]
+      Linf_Lmax_ratios[i,12]<-k_t0_jits[2]
+      
+      #Linf_Lmax_ratios[i,4]<-(growth.out.parms[1]/max(rand.lts))/(growth.out.parms[1]/Linf.in)    
+    }
+  }
+  return(Linf_Lmax_ratios)
+}
+
+Linf_Lmax_plots<-function(Linf_Lmax_ratios)
+{
+  ratios<-Linf_Lmax_ratios[,1:3]
+  gg_Linf_Lmax_ratios<-melt(ratios)
+  colnames(gg_Linf_Lmax_ratios)<-c("Label","Ratio")
+  ggplot(gg_Linf_Lmax_ratios,aes(Ratio,color=Label))+ 
+    geom_density(lwd=2)+
+    geom_vline(xintercept=c(1,median(Linf_Lmax_ratios$estLinf_TLinf,na.rm=TRUE)), color=c("red","black"))+
+    xlim(0,2)
+}
+
+Linf_plots<-function(Linf_Lmax_ratios)
+{
+  ratios<-Linf_Lmax_ratios[,3]
+  #gg_Linf_Lmax_ratios<-melt(ratios)
+  #colnames(gg_Linf_Lmax_ratios)<-c("Label","Ratio")
+  ggplot(as.data.frame(Linf_Lmax_ratios[,3]),aes(Linf_Lmax_ratios[,3]))+ 
+    geom_density(lwd=2)+
+    geom_vline(xintercept=1, color=c("red"))+
+    xlim(0,2)+
+    xlab("Ratio of Est. Linf to True Linf")
+}
+
 #############
 
 #Upload VBGF and tmax values from FishBase
@@ -52,7 +135,7 @@ VBGF.fb.out_noNA$K<-as.numeric(VBGF.fb.out_noNA$K)
 VBGF.fb.out_noNA$tmax<-as.numeric(VBGF.fb.out_noNA$tmax)
 #Save as R object
 save(VBGF.fb.out_noNA,file="C:/Users/Jason.Cope/Documents/Current Action/Publications/tmax vs Linf/VBGF_fb_out_noNA.rds")
-load("C:/Users/Jason.Cope/Documents/Current Action/Publications/tmax vs Linf/VBGF_fb_out_noNA.rds")
+load("C:/Users/Jason.Cope/Documents/Github/Linf_Lmax_Amax/VBGF_fb_out_noNA.rds")
 
 #Extract Lmax values from FishBase
 Lmax.spp.fb<-popchar(fb.spp.parms$Species)
@@ -155,96 +238,155 @@ rand.ages<-c(rand.ages.5,rand.ages.15,rand.ages.25,rand.ages.35,rand.ages.50)
 
 #rand.ages<-round(rtruncnorm(1000,2,10,10,10),0)
 
-
-
-#rand.VBGF(Linf.in,K.in,t0.in,Amax.in,0.1)
-#summary(rand.VBGF(Linf.in,K.in,t0.in,rep(Amax.in,100),0.1))
-
-#Linf.in<-Lmax_less$Linf[1]
-#K.in<-Lmax_less$K[1]
-#t0.in<-0
-#CV.in<-0.1
-#Amax.in<-Lmax_less$tmax[1]
-
-
-Comp.Lmax.Linf<-function(n=100,Nsim=500,Linf.in,K.in,t0.in,CV.in,Amax.in,lowage,maxage)
-{
-  Linf_Lmax_ratios<-data.frame(TLinf_Lmax=NA,estLinf_Lmax=NA,estLinf_TLinf=NA,Linf.true=NA,Linf_est=NA,Lmax=NA, Amin=NA, Amax=NA,Kest=NA,K_jit=NA,toest=NA,t0_jit=NA)
-  for(i in 1:Nsim)
-  {
-    rand.ages<-rlnormTrunc(n, meanlog = log(Amax.in), sdlog = 1, min = lowage,max = maxage)
-    #rand.ages.trunc<-rand.ages[rand.ages>lowage]
-    rand.lts<-rand.VBGF(Linf.in,K.in,t0.in,rand.ages,CV.in)
-    #  Age_Lts<-data.frame(Ages=rand.ages,Lts=rand.lts)
-    #  plot(rand.ages,rand.lts)
-    #Fit VBGF curve
-    k_t0_jits<-c(jitter(K.in,amount=0.05),jitter(t0.in,amount=0.05))
-    growth.out<-try(growth(size=rand.lts,age=round(rand.ages,0),Sinf=max(rand.lts),K=k_t0_jits[1],t0=0,graph=FALSE,control=list(maxiter=10000,minFactor=100,tol=1e-5))$vout) #k_t0_jits[2]
-    #vbmod <- Lts ~ Linf * (1 - exp(-K * (Ages - t0)))
-    #vbgf.parms<-list(Linf=20,K=0.1,t0=0)
-    #vbgf.fit<-nls(vbmod,data=Age_Lts,start=vbgf.parms)
-    if(any(growth.out=="Fit failed"))
-    {Linf_Lmax_ratios[i,]=NA}
-    
-    if(any(growth.out!="Fit failed"))
-    {
-      growth.out.parms<-growth.out$m$getPars()
-      Linf_Lmax_ratios[i,1]<-Linf.in/max(rand.lts)
-      Linf_Lmax_ratios[i,2]<-growth.out.parms[1]/max(rand.lts)
-      Linf_Lmax_ratios[i,3]<-growth.out.parms[1]/Linf.in
-      Linf_Lmax_ratios[i,4]<-Linf.in
-      Linf_Lmax_ratios[i,5]<-growth.out.parms[1]
-      Linf_Lmax_ratios[i,6]<-max(rand.lts)
-      Linf_Lmax_ratios[i,7]<-min(rand.ages.trunc)
-      Linf_Lmax_ratios[i,8]<-max(rand.ages.trunc)
-      Linf_Lmax_ratios[i,9]<-growth.out.parms[2]
-      Linf_Lmax_ratios[i,10]<-k_t0_jits[1]
-      Linf_Lmax_ratios[i,11]<-growth.out.parms[3]
-      Linf_Lmax_ratios[i,12]<-k_t0_jits[2]
-      
-      #Linf_Lmax_ratios[i,4]<-(growth.out.parms[1]/max(rand.lts))/(growth.out.parms[1]/Linf.in)    
-    }
-  }
-  return(Linf_Lmax_ratios)
-}
-
-Linf_Lmax_plots<-function(Linf_Lmax_ratios)
-{
-    ratios<-Linf_Lmax_ratios[,1:3]
-    gg_Linf_Lmax_ratios<-melt(ratios)
-    colnames(gg_Linf_Lmax_ratios)<-c("Label","Ratio")
-    ggplot(gg_Linf_Lmax_ratios,aes(Ratio,color=Label))+ 
-      geom_density(lwd=2)+
-      geom_vline(xintercept=1, color="red")+
-      xlim(0,2)
-}
-
-
-###########
-Linf.in<-75
-K.in<-0.05
-t0.in<--3
+#############################
+### Plot VBGF and samples ###
+#############################
+Linf.in<-60
+M<-0.1
+K.in<-M/0.5
+t0.in<--1
 CV.in<-0.1
 
-Amax.in<-10
-lowage<-2
-maxage<-10
-##########
-Linf_Lmax_ratios<-Comp.Lmax.Linf(n=500,Nsim=500,Linf.in=Linf.in,K.in=K.in,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage)
+lowage<-0.1
+maxage<-5.4/M
+maxlt<-Linf.in*0.9
+
+ages.full<-rlnormTrunc(1000, meanlog = log(Amax.in), sdlog = 1, min = lowage,max = maxage)
+rand.lts.full<-rand.VBGF(Linf.in,K.in,t0.in,ages.full,CV.in)
+
+age.lt.dat<-data.frame(Ages=round(ages.full,0),Lengths=round(rand.lts.full,0),Sample="Full")
+
+age.lt.dat.Alim<-age.lt.dat[age.lt.dat$Ages<maxage*0.5,]
+age.lt.dat.Alim$Sample<-paste0("Age ",round(maxage*0.5,0))
+age.lt.dat.Llim<-age.lt.dat[age.lt.dat$Lengths<maxlt,]
+age.lt.dat.Llim$Sample<-paste0("Lt ",round(maxlt,0))
+age.lt.dat.AlimLlim<-age.lt.dat[age.lt.dat$Ages<maxage*0.5&age.lt.dat$Lengths<maxlt,]
+age.lt.dat.AlimLlim$Sample<-paste0("Age ",maxage*0.5,", Lt ",maxlt)
+age.lt.dat.Alow<-age.lt.dat[age.lt.dat$Ages<maxage*0.2,]
+age.lt.dat.Alow$Sample<-"20%Amax"
+
+age.lt.dat.all<-rbind(age.lt.dat,age.lt.dat.Alim,age.lt.dat.Llim,age.lt.dat.AlimLlim)
+age.lt.dat.all$Sample<-factor(age.lt.dat.all$Sample,levels=c("Full",paste0("Age ",round(maxage*0.5,0)),paste0("Lt ",round(maxlt,0)),paste0("Age ",maxage*0.5,", Lt ",maxlt)))
+
+vbgf.parms.all<-nls(age.lt.dat$Lengths~Linf * (1 - exp(-k * (age.lt.dat$Ages - t0))),data=age.lt.dat,start=list(Linf=max(age.lt.dat$Lengths),k=0.1,t0=0))
+vbgf.parms.Alim<-nls(age.lt.dat.Alim$Lengths~Linf * (1 - exp(-k * (age.lt.dat.Alim$Ages - t0))),data=age.lt.dat.Alim,start=list(Linf=max(age.lt.dat.Alim$Lengths),k=0.1,t0=0))
+vbgf.parms.Llim<-nls(age.lt.dat.Llim$Lengths~Linf * (1 - exp(-k * (age.lt.dat.Llim$Ages - t0))),data=age.lt.dat.Llim,start=list(Linf=max(age.lt.dat.Llim$Lengths),k=0.1,t0=0))
+vbgf.parms.AlimLlim<-nls(age.lt.dat.AlimLlim$Lengths~Linf * (1 - exp(-k * (age.lt.dat.AlimLlim$Ages - t0))),data=age.lt.dat.AlimLlim,start=list(Linf=max(age.lt.dat.AlimLlim$Lengths),k=0.1,t0=0))
+vbgf.parms.Alow<-nls(age.lt.dat.Alow$Lengths~Linf * (1 - exp(-k * (age.lt.dat.Alow$Ages - t0))),data=age.lt.dat.Alow,start=list(Linf=max(age.lt.dat.Alow$Lengths),k=0.1,t0=0))
+
+#Fitting all samples
+ggplot(age.lt.dat,aes(Ages,Lengths,colour = Sample))+
+  geom_point()+
+  scale_colour_manual(values = c("red"))+
+  theme_bw()+
+  geom_function(fun = function(x) Linf.in * (1 - exp(-K.in * (x - t0.in))), colour = "black",lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.all$m$getPars()[1] * (1 - exp(-vbgf.parms.all$m$getPars()[2] * (x - vbgf.parms.all$m$getPars()[3]))), colour = c("red"),lwd=1.25)
+#Fitting A50%
+ggplot(subset(age.lt.dat.all,Sample %in% c("Full",paste0("Age ",round(maxage*0.5,0)))),aes(Ages,Lengths,colour = Sample))+
+  geom_point()+
+  scale_colour_manual(values = c("red","pink"))+
+  theme_bw()+
+  geom_function(fun = function(x) Linf.in * (1 - exp(-K.in * (x - t0.in))), colour = c("black"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.all$m$getPars()[1] * (1 - exp(-vbgf.parms.all$m$getPars()[2] * (x - vbgf.parms.all$m$getPars()[3]))), colour = c("red"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.Alim$m$getPars()[1] * (1 - exp(-vbgf.parms.Alim$m$getPars()[2] * (x - vbgf.parms.Alim$m$getPars()[3]))), colour = c("pink"),lwd=1.25)
+#Fitting Linf90%
+ggplot(subset(age.lt.dat.all,Sample %in% c("Full",paste0("Lt ",round(maxlt,0)))),aes(Ages,Lengths,colour = Sample))+
+  geom_point()+
+  scale_colour_manual(values = c("red","darkgreen"))+
+  theme_bw()+
+  geom_function(fun = function(x) Linf.in * (1 - exp(-K.in * (x - t0.in))), colour = c("black"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.all$m$getPars()[1] * (1 - exp(-vbgf.parms.all$m$getPars()[2] * (x - vbgf.parms.all$m$getPars()[3]))), colour = c("red"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.Llim$m$getPars()[1] * (1 - exp(-vbgf.parms.Llim$m$getPars()[2] * (x - vbgf.parms.Llim$m$getPars()[3]))), colour = c("darkgreen"),lwd=1.25)
+#Fitting A50% and Linf90%
+ggplot(subset(age.lt.dat.all,Sample %in% c("Full",paste0("Age ",maxage*0.5,", Lt ",maxlt))),aes(Ages,Lengths,colour = Sample))+
+  geom_point()+
+  scale_colour_manual(values = c("red","blue"))+
+  theme_bw()+
+  geom_function(fun = function(x) Linf.in * (1 - exp(-K.in * (x - t0.in))), colour = c("black"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.all$m$getPars()[1] * (1 - exp(-vbgf.parms.all$m$getPars()[2] * (x - vbgf.parms.all$m$getPars()[3]))), colour = c("red"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.AlimLlim$m$getPars()[1] * (1 - exp(-vbgf.parms.AlimLlim$m$getPars()[2] * (x - vbgf.parms.AlimLlim$m$getPars()[3]))), colour = c("blue"),lwd=1.25)
+#Fit all scenarios
+ggplot(age.lt.dat.all,aes(Ages,Lengths,colour = Sample))+
+  geom_point()+
+  scale_colour_manual(values = c("red","pink" , "darkgreen","blue"))+
+  theme_bw()+
+  geom_function(fun = function(x) Linf.in * (1 - exp(-K.in * (x - t0.in))), colour = "black",lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.all$m$getPars()[1] * (1 - exp(-vbgf.parms.all$m$getPars()[2] * (x - vbgf.parms.all$m$getPars()[3]))), colour = "red",lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.Alim$m$getPars()[1] * (1 - exp(-vbgf.parms.Alim$m$getPars()[2] * (x - vbgf.parms.Alim$m$getPars()[3]))), colour = "pink",lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.Llim$m$getPars()[1] * (1 - exp(-vbgf.parms.Llim$m$getPars()[2] * (x - vbgf.parms.Llim$m$getPars()[3]))), colour ="darkgreen",lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.AlimLlim$m$getPars()[1] * (1 - exp(-vbgf.parms.AlimLlim$m$getPars()[2] * (x - vbgf.parms.AlimLlim$m$getPars()[3]))), colour = "blue",lwd=1.25)
+#Low ages and lengths  
+age.lt.dat.comp.Alow<-rbind(age.lt.dat,age.lt.dat.Alow)
+age.lt.dat.comp.Alow$Sample<-factor(age.lt.dat.comp.Alow$Sample,levels=c("Full","20%Amax"))
+ggplot(age.lt.dat.comp.Alow,aes(Ages,Lengths,colour = Sample))+
+  geom_point()+
+  scale_colour_manual(values = c("red","orange"))+
+  theme_bw()+
+  geom_function(fun = function(x) Linf.in * (1 - exp(-K.in * (x - t0.in))), colour = c("black"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.all$m$getPars()[1] * (1 - exp(-vbgf.parms.all$m$getPars()[2] * (x - vbgf.parms.all$m$getPars()[3]))), colour = c("red"),lwd=1.25)+
+  geom_function(fun = function(x) vbgf.parms.Alow$m$getPars()[1] * (1 - exp(-vbgf.parms.Alow$m$getPars()[2] * (x - vbgf.parms.Alow$m$getPars()[3]))), colour = c("orange"),lwd=1.25)
+
+
+
+###################
+### Simulations ###
+###################
+
+Linf.in<-60
+M<-0.2
+K.in.05<-M/0.5
+K.in.1.5<-M/1.5
+K.in.2<-M/2
+t0.in<--1
+CV.in<-0.1
+
+lowage<-0.1
+maxage<-5.4/M
+maxage<-5.4/M/2
+maxlt<-Linf.in*0.9
+maxlt<-100
+
+#M/k=0.5
+Linf_Lmax_ratios_Mk05.N100<-Comp.Lmax.Linf(n=100,Nsim=500,Linf.in=Linf.in,K.in=K.in.05,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage,maxlt)
+Linf_Lmax_ratios_Mk05.N200<-Comp.Lmax.Linf(n=200,Nsim=500,Linf.in=Linf.in,K.in=K.in.05,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage,maxlt)
+Linf_Lmax_ratios_Mk05.N500<-Comp.Lmax.Linf(n=500,Nsim=500,Linf.in=Linf.in,K.in=K.in.05,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage,maxlt)
+ratios100<-data.frame(Ratio=Linf_Lmax_ratios_Mk05.N100[,3],Label="Mk_05_N100")
+ratios200<-data.frame(Ratio=Linf_Lmax_ratios_Mk05.N200[,3],Label="Mk_05_N200")
+ratios500<-data.frame(Ratio=Linf_Lmax_ratios_Mk05.N500[,3],Label="Mk_05_N500")
+gg_Linf_ratios<-rbind(ratios100,ratios200,ratios500)
+ggplot(gg_Linf_ratios,aes(Ratio,color=Label))+ 
+  geom_density(lwd=2)+
+  geom_vline(xintercept=c(1), color="red")+
+  xlim(0,2)
+
+#M/k=1.5
+Linf_Lmax_ratios_Mk15<-Comp.Lmax.Linf(n=200,Nsim=500,Linf.in=Linf.in,K.in=K.in.1.5,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage,maxlt)
+
+#M/k=2.5
+Linf_Lmax_ratios_Mk20<-Comp.Lmax.Linf(n=200,Nsim=500,Linf.in=Linf.in,K.in=K.in.2,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage,maxlt)
+
+ratiosmk05<-data.frame(Ratio=Linf_Lmax_ratios_Mk05.N200[,3],Label="Mk_0.5")
+ratiosmk15<-data.frame(Ratio=Linf_Lmax_ratios_Mk15[,3],Label="Mk_1.5")
+ratiosmk20<-data.frame(Ratio=Linf_Lmax_ratios_Mk20[,3],Label="Mk_2")
+gg_Linf_ratios<-rbind(ratiosmk05,ratiosmk15,ratiosmk20)
+ggplot(gg_Linf_ratios,aes(Ratio,color=Label))+ 
+  geom_density(lwd=2)+
+  geom_vline(xintercept=c(1), color="red")+
+  xlim(0,2)
+
+
+Linf_plots(Linf_Lmax_ratios)
 Linf_Lmax_plots(Linf_Lmax_ratios)
 summary(Linf_Lmax_ratios[])  
 
 
-##########
-Linf.in<-50
-K.in<-0.1
-t0.in<--2
-CV.in<-0.1
 
-Amax.in<-10
-lowage<-1
-maxage<-10
-###########
+
+
+
+
+
+
 
 Linf_Lmax_grid<-data.frame(TLinf_Lmax =NA,estLinf_TLinf=NA,K=NA)
 K_vec<-seq(0.05,1,0.05)
@@ -259,24 +401,6 @@ for(i in 1:length(K_vec))
 Linf_Lmax_plots(Linf_Lmax_ratios)
 summary(Linf_Lmax_ratios[])  
 
-###########
-Linf.in<-20
-K.in<-0.3
-t0.in<--1
-CV.in<-0.1
-
-Amax.in<-5
-lowage<-1
-maxage<-3
-###########
-Linf_Lmax_ratios<-Comp.Lmax.Linf(n=500,Nsim=500,Linf.in=Linf.in,K.in=K.in,t0.in=t0.in,CV.in=CV.in,Amax.in=Amax.in,lowage=lowage,maxage=maxage)
-Linf_Lmax_plots(Linf_Lmax_ratios)
-summary(Linf_Lmax_ratios[])  
-
-
-
-
-#plot(Linf_Lmax_ratios[,2],Linf_Lmax_ratios[,3])
 
 
 
@@ -290,24 +414,3 @@ abs(Linf_Lmax_ratios$estLinf_TLinf-1)>0.05
 
 
 
-
-#Reduce sampling
-ageless<-25
-ages.less<-rand.ages[rand.ages<ageless]
-Lts.less<-Lts.rand[rand.ages<ageless]
-growth(size=Lts.less,age=ages.less,Sinf=40,K=0.15,t0=0)$vout
-
-#Max length < Linf
-ltsless<-40
-Lts.lessLinf<-Lts.rand[rand.lts<ltsless]
-ages.lessLinf<-rand.ages[rand.lts<ltsless]
-growth(size=Lts.lessLinf,age=ages.lessLinf,Sinf=50,K=0.1,t0=0)$vout
-
-#Max length < Linf
-Lts.lessLinfage<-Lts.rand[rand.lts<ltsless&rand.ages<ageless]
-ages.lessLinfage<-rand.ages[rand.lts<ltsless&rand.ages<ageless]
-growth(size=Lts.lessLinfage,age=ages.lessLinfage,Sinf=50,K=0.1,t0=0)$vout
-
-
-#Compare distribution of parameters when Linf ratios > 1
-xx<-VBGF_tmax_Lmax[VBGF_tmax_Lmax$Linf__Linf_tmax>Linf_Amax_ratio &VBGF_tmax_Lmax$Linf_Lmax>Linf_Lmax_ratio,]
